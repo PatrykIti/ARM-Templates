11dda3c9e36c5f6d39c9a0fa3e7bf0a43bc56110
PatrykIti <ciechanski.patryk@outlook.com>
2021-03-10 10:03:59 +0100

    update2

--- SQL/Set Auditing to EventHub/profileHelper.ps1
+++ SQL/Set Auditing to EventHub/profileHelper.ps1
@@ -3,5 +3,3 @@ New-AzureRmResourceGroup -Name $rg -Location northeurope
 
 $date = get-date -Format yyyyMMdd-HHmmss
 New-AzureRmResourceGroupDeployment -Name "new-sql-deploy-$date" -ResourceGroupName $rg -TemplateFile 'main.json' -serverName 'deveunsql08'
-
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile 'main.json' -serverName 'deveunsql03'
