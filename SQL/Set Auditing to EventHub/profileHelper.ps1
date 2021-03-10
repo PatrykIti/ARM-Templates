00f9f20357a155a92d43059626dc8a07363382b2
PatrykIti <ciechanski.patryk@outlook.com>
2021-03-10 10:56:32 +0100

    update

--- SQL/Set Auditing to EventHub/profileHelper.ps1
+++ SQL/Set Auditing to EventHub/profileHelper.ps1
@@ -1,5 +1,8 @@
-$rg = "deveunsqltest8"
+$rg = "deveunsqltest01"
+$ehn = Get-AzureRmResource -Name "*eventhubnamespace*"
+$ehnAuthRule = (Get-AzureRmEventHubAuthorizationRule -ResourceGroupName $ehn.ResourceGroupName -Namespace $ehn.Name -Name "RootManageSharedAccessKey").id
+
 New-AzureRmResourceGroup -Name $rg -Location northeurope
 
 $date = get-date -Format yyyyMMdd-HHmmss
-New-AzureRmResourceGroupDeployment -Name "new-sql-deploy-$date" -ResourceGroupName $rg -TemplateFile 'main.json' -serverName 'deveunsql08'
+New-AzureRmResourceGroupDeployment -Name "new-sql-deploy-$date" -ResourceGroupName $rg -TemplateFile 'main.json' -serverName 'deveunsql01' -sqlDBName "SampleDB" -administratorLogin "azureadmin" -administratorLoginPassword "adminadmin123!" -eventHubName "eh-for-sql" -eventHubAuthKey $ehnAuthRule
